21b2517de051393d9e3993fe0ff1602667ae954f
JATINGARG25 <gargjatin519@gmail.com>
2024-02-10 12:38:20 +0530

    add more files

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+a = 15
+b = 56
+
+print("Addition of Numbers is ",a+b)
+print("Subtraction of Numbers is ",a-b)
+print("Multiplication of Numbers is ",a*b)
+print("Division of Numbers is ",a/b)
+print("Exponentionl of Numbers is ",a**b)
+print("Floor Divison of Numbers is ",a//b)
+print("Modolus of Numbers is ",a%b)
+
+
+
+
---
+++ data_types.py
@@ -0,0 +1,7 @@
+a = 8
+b = "Jatin"
+c = complex(7,8)
+
+print(c)
+print("The type of a is ",type(a))
+print("The type of a is ",type(c))
